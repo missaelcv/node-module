43f5c331270ade420d717e2700f72862082e4ee5
missaelcv <missaelcv0220@gmail.com>
2022-06-08 22:56:24 -0400

    function escribir un nuevo archivo.txt

--- modulos/fs.js
+++ modulos/fs.js
@@ -9,4 +9,16 @@ function Leer(ruta, cb){
     })
 }
 
-Leer (__dirname + '/archivo.txt')
+function Escribir(ruta, contenido, cb){
+    fs.writeFile(ruta,contenido, function(err) {
+        if (err) {
+            console.error('no he modido escribir',err);
+        }else {
+            console.log('se ha escrito correcto');
+        }
+    })
+
+}
+
+Escribir (__dirname + '/archivo1.txt','soy el tema nuevo', console.log());
+//Leer (__dirname + '/archivo.txt')
